a9c2037a9464ed35c0d3a7ab730d59eb8c77fd7b
Doron Orenstein <jazziseverywhere@gmail.com>
2017-04-06 22:47:03 -0500

    got date display working right, just need to fix formatting

--- src/components/EventDateFilter/EventDateFilter.js
+++ src/components/EventDateFilter/EventDateFilter.js
@@ -1,14 +1,36 @@
 import React, { PropTypes } from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 import styles from './EventDateFilter.sass';
+
+console.log('styles', styles);
+
+const dateFormatToday = 'MMM D';
+const dateFormatMain = 'ddd MMM D';
+
+function isStartDateToday(startDate) {
+  const dateToday = moment();
+  return moment(startDate).isSame(dateToday, 'day');
+}
+
+function generateDateFormat(startDate){
+
+  if (isStartDateToday(startDate)) {
+    return dateFormatToday;
+  }
+
+  return dateFormatMain;
+}
 
 const EventDateFilter = (props) => {
   const { startDate, updateFilters, placeholderText, isClearable } = props;
 
   return (
     <div className={styles.dateFilterText}>
+      <span className={styles.todayText}>{isStartDateToday(startDate) && 'TODAY'}</span>
       <DatePicker
+        dateFormat={generateDateFormat(startDate)}
         selected={startDate}
         onChange={date => updateFilters({ startDate: date })}
         isClearable={isClearable}
@@ -19,8 +41,8 @@ const EventDateFilter = (props) => {
 };
 
 EventDateFilter.defaultProps = {
-  placeholderText: 'Select a date',
-  isClearable: true
+  placeholderText: `TODAY ${moment().format('MMM D')}`,
+  isClearable: false
 };
 
 EventDateFilter.propTypes = {
